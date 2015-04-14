97047b32a0c20ec07f3c0dfc9bb9ae7d0906c813
Cody Hiar <chiar@hybridforge.com>
2015-04-14 16:14:03 -0600

    Finishing up part III, now moving onto databases

--- app/views.py
+++ app/views.py
@@ -14,6 +14,6 @@ def login():
         flash('Login requested for OpenID="%s", remember_me=%s' %
             (form.openid.data, str(form.remember_me.data)))
         return redirect('/index')
-    return render_template('login.html', title='Sign in', form=form)
+    return render_template('login.html', title='Sign in', form=form, providers=app.config['OPENID_PROVIDERS'])
 
 
